d7ee02d358c0c106d67715c19b7de3bb84283f58
tushar-chadha <tushar_chadha@hotmail.com>
2026-06-05 19:34:50 +0530

    Initial Commit

---
+++ RemoteRecruit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RemoteRecruit
+//
+//  Created by tushar on 05/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RemoteRecruit/RemoteRecruitApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteRecruitApp.swift
+//  RemoteRecruit
+//
+//  Created by tushar on 05/06/26.
+//
+
+import SwiftUI
+
+@main
+struct RemoteRecruitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
